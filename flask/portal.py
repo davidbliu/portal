24d10f3337b3c5acc43dd384dfb30e53b90ed827
David Liu <davidbliu@gmail.com>
2015-04-08 15:45:35 -0700

    flask started

--- flask/portal.py
+++ flask/portal.py
@@ -1,4 +1,18 @@
+from flask import Flask, request, jsonify, render_template
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('course_webpage.html')
+
+@app.route("/past_announcements")
+def past_announcements():
+	return render_template('past_announcements.html')
 
 if __name__ == "__main__":
-	print 'running members portal'
+    host = '0.0.0.0'
+	port = 5139
+
+	app.run(host = host, port = port, debug=True)
